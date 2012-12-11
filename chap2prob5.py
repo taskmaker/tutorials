3e292d322d11ca4ea631ab482fcda43a51563b04
Gerard MacManus <gmacmanus@student.stbedes.school.nz>
2012-12-11 22:43:55 +1300

    Update chap2prob5.py

--- chap2prob5.py
+++ chap2prob5.py
@@ -1,8 +1,8 @@
-# Chapter 2 Problem 5 - robot distance travelled - declare own PI version
-PI = 3.142
+# Chapter 2 Problem 5 - robot distance travelled - math.pi version
+import math
 RADIUS = 2.7 # of wheel, in cm
 rotations = 2
 #robot will travel the circumference of its wheel (pi * d) with each rotation
-distance = PI * RADIUS * 2 * rotations
-print("Rotations:",rotations)
-print("Distance travelled:",distance,"cm")
+distance = math.pi * RADIUS * 2 * rotations
+print("Rotations:", rotations)
+print("Distance travelled:", distance,"cm")
